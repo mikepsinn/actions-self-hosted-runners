3b9f583146243d50fe4fedfa8559d809065fc046
Mike P. Sinn <m@quantimodo.com>
2023-01-12 00:58:36 -0600

    source /home/vagrant/actions-runners/cd-api/run.sh

--- actions-runners.sh
+++ actions-runners.sh
@@ -3,5 +3,7 @@
 set -eu -o pipefail # -x: is for debugging
 set -x
 export RUNNER_ALLOW_RUNASROOT="1"
-nohup "/home/vagrant/actions-runners/cd-api/run.sh" &
+sudo chown -R vagrant:vagrant /home/vagrant/actions-runners
+#nohup "/home/vagrant/actions-runners/cd-api/run.sh" &
 nohup "/home/vagrant/actions-runners/cd-ionic/run.sh" &
+source /home/vagrant/actions-runners/cd-api/run.sh
